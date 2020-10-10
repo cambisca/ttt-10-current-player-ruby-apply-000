a54f8b9264ef94901c08cd78fbc531ed724cdd60
Cameron Bisca <cjbisca@gmail.com>
2020-10-10 23:47:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,11 +4,11 @@ end
 
 def turn_count(board)
   counter = 0
-  board.each {|space|
+  board.each do |space|
     if space == "X" || space == "O"
       counter += 1   
     end
-  }
+  end 
   counter
 end
 
